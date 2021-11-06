9318b6d07c890d696d75d695adf21990c369d0f0
NolanGougeon <gnolan@hotmail.fr>
2021-11-06 18:35:45 +0100

    Coins rotation

---
+++ GameJamAto/Assets/Scripts/Coins.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
