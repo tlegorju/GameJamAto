ae4883a0fcfe8e87ad5d63fccbdde88151be7454
tlegorju <thibault.legorju@gmail.com>
2021-11-06 13:11:01 +0100

    Managers WIP

---
+++ GameJamAto/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+
+    [SerializeField] private int LIFE_AT_START = 10;
+
+    private int lifeLeft = 10;
+    public int LifeLeft { get { return lifeLeft; } }
+
+    //[SerializeField] private PlayerController player;
+
+
+    private void Awake()
+    {
+        if(instance!=null)
+        {
+            Destroy(this);
+            return;
+        }
+        instance = this;
+    }
+
+    public void Reset()
+    {
+        lifeLeft = LIFE_AT_START;
+    }
+
+    public void TakeDamages(int damages)
+    {
+        lifeLeft = Mathf.Max(0, lifeLeft - damages);
+        //UIMananager.Instance.UpdateLifeLeft(lifeLeft);
+        if(CheckForGameOver())
+        {
+            TriggerGameOver();
+        }
+    }
+
+    public bool CheckForGameOver()
+    {
+        return lifeLeft <= 0;
+    }
+
+    public void TriggerGameOver()
+    {
+        Debug.Log("Game Over");
+
+        //Disable player control
+        //Disable ennemies movement
+
+        //Play defeat sound
+        //Show game over UI
+    }
+}
